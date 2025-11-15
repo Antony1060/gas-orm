70c25ab96000d60ed7d57eed4ffeb64789767ba9
Antony1060 <antoniostignjedec@gmail.com>
2025-11-15 01:44:21 +0100

    small change

--- gas-macros/src/text_util.rs
+++ gas-macros/src/text_util.rs
@@ -2,12 +2,7 @@ pub(crate) fn pascal_to_snake_case(src: &str) -> String {
     let mut out = String::with_capacity(src.len() + src.len() / 2);
 
     for (idx, char) in src.chars().enumerate() {
-        if idx == 0 {
-            out.push(char.to_ascii_lowercase());
-            continue;
-        }
-
-        if char.is_ascii_uppercase() {
+        if idx != 0 && char.is_ascii_uppercase() {
             out.push('_');
         }
 
