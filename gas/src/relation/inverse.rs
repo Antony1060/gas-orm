64199707df49518675741bb208e0751b2e1ea95e
Antony1060 <antoniostignjedec@gmail.com>
2025-12-10 18:19:49 +0100

    things

--- gas/src/relation/inverse.rs
+++ gas/src/relation/inverse.rs
@@ -1,13 +1,16 @@
 use crate::connection::PgExecutionContext;
+use crate::error::GasError;
 use crate::internals::{AsPgType, IsOptional, PgParam, PgType};
+use crate::ops::select::SelectBuilder;
 use crate::row::{FromRowNamed, ResponseCtx, Row};
-use crate::{GasResult, ModelMeta};
+use crate::{GasResult, ModelMeta, ModelOps};
 use std::ops::Deref;
-use std::sync::Arc;
 
 #[allow(dead_code)]
 #[derive(Debug, Clone, Default)]
 pub struct InverseRelation<Ret: Clone + Default, const FORWARD_FIELD_INDEX: usize> {
+    // TODO: make generic
+    parent_fk_value: i64,
     loaded: bool,
     items: Ret,
 }
@@ -23,8 +26,9 @@ pub trait InverseRelationTypeOps {
     const TYPE: InverseRelationType;
 }
 
-type ToManyContainer<M> = Box<[Arc<M>]>;
-type ToOneContainer<M> = Option<Arc<M>>;
+// TODO: these will probably cause depth problems, figure out later
+type ToManyContainer<M> = Box<[M]>;
+type ToOneContainer<M> = Option<M>;
 
 impl<M: ModelMeta> InverseRelationTypeOps for Vec<M> {
     type Inner = ToManyContainer<M>;
@@ -66,6 +70,8 @@ impl<M: ModelMeta, const FORWARD_FIELD_INDEX: usize> FromRowNamed
     fn from_row_named(_ctx: &ResponseCtx, _row: &Row, _name: &str) -> GasResult<Self> {
         // TODO:
         Ok(Self {
+            // TODO: somehow get the name of the field inside and make a row get
+            parent_fk_value: 0,
             loaded: false,
             items: ToManyContainer::<M>::default(),
         })
@@ -78,6 +84,8 @@ impl<M: ModelMeta, const FORWARD_FIELD_INDEX: usize> FromRowNamed
     fn from_row_named(_ctx: &ResponseCtx, _row: &Row, _name: &str) -> GasResult<Self> {
         // TODO:
         Ok(Self {
+            // TODO: somehow get the name of the field inside and make a row get
+            parent_fk_value: 0,
             loaded: false,
             items: ToOneContainer::<M>::default(),
         })
@@ -120,14 +128,20 @@ pub trait InverseRelationOps<Ret> {
 impl<M: ModelMeta, const FORWARD_FIELD_INDEX: usize> InverseRelationOps<ToManyContainer<M>>
     for InverseRelation<ToManyContainer<M>, FORWARD_FIELD_INDEX>
 {
+    // NOTE: untested
     async fn reload<'a, E: PgExecutionContext>(
         &'a mut self,
-        _ctx: E,
+        ctx: E,
     ) -> GasResult<&'a ToManyContainer<M>>
     where
         M: 'a,
     {
-        todo!()
+        let select = make_lazy_inverse_query::<i64, M, FORWARD_FIELD_INDEX>(self.parent_fk_value)?;
+
+        let resp = select.find_all(ctx).await?;
+        self.loaded = true;
+        self.items = resp.into_boxed_slice();
+        Ok(&self.items)
     }
 }
 
@@ -136,11 +150,37 @@ impl<M: ModelMeta, const FORWARD_FIELD_INDEX: usize> InverseRelationOps<ToOneCon
 {
     async fn reload<'a, E: PgExecutionContext>(
         &'a mut self,
-        _ctx: E,
+        ctx: E,
     ) -> GasResult<&'a ToOneContainer<M>>
     where
         M: 'a,
     {
-        todo!()
+        let select = make_lazy_inverse_query::<i64, M, FORWARD_FIELD_INDEX>(self.parent_fk_value)?;
+
+        let resp = select.find_one(ctx).await?;
+        self.loaded = true;
+        self.items = resp;
+        Ok(&self.items)
     }
+}
+
+fn make_lazy_inverse_query<Fk, M: ModelMeta, const FIELD_INDEX: usize>(
+    parent_fk_value: Fk,
+) -> GasResult<SelectBuilder<M>>
+where
+    PgParam: From<Fk>,
+{
+    let field = M::FIELDS
+        .get(FIELD_INDEX)
+        .ok_or_else(|| GasError::InvalidRelation)?;
+
+    let mut select = M::query();
+    unsafe {
+        select = select.raw_filter(
+            format!("{}=?", field.full_name),
+            &[PgParam::from(parent_fk_value)],
+        );
+    }
+
+    Ok(select)
 }
