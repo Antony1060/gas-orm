7e43e0c1176fe789994ef48f8a51e191265c2067
Antony1060 <antoniostignjedec@gmail.com>
2025-12-19 01:54:17 +0100

    things

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,6 +1,19 @@
-use ra_ap_load_cargo::{load_workspace_at, LoadCargoConfig, ProcMacroServerChoice};
-use ra_ap_project_model::{CargoConfig, RustLibSource};
+use object::{Object, ObjectSection};
+use std::fs;
+use std::mem::MaybeUninit;
 use std::path::Path;
+
+#[derive(Debug)]
+pub struct FieldMeta {
+    pub table_name: *const str,
+    pub full_name: *const str,
+    pub name: *const str,
+    pub alias_name: *const str,
+    pub struct_name: *const str,
+    pub pg_type: gas::internals::PgType,
+    pub flags: gas::FieldFlags,
+    pub index: usize,
+}
 
 fn main() -> anyhow::Result<()> {
     let args = std::env::args().skip(1);
@@ -10,45 +23,53 @@ fn main() -> anyhow::Result<()> {
         return Err(anyhow::anyhow!("empty argument"));
     }
 
-    let project_path = &args[0];
-    let project_path = Path::new(project_path);
-    let cargo_toml_path = project_path.join("Cargo.toml");
+    let binary_path = &args[0];
+    let binary_path = Path::new(binary_path);
 
-    if !cargo_toml_path.exists() {
+    if !binary_path.exists() {
         return Err(anyhow::anyhow!(
-            "Cargo.toml not found in {}",
-            project_path.display()
+            "binary path not found in {}",
+            binary_path.display()
         ));
     }
 
-    dbg!(&cargo_toml_path);
+    dbg!(&binary_path);
 
-    // let abs_path = AbsPathBuf::try_from(absolute_utf8(cargo_toml_path)?);
-    //
-    // dbg!(&abs_path);
+    let binding = fs::read(binary_path)?;
+    let file = object::File::parse(&*binding)?;
 
-    // let manifest = ProjectManifest::from_manifest_file(abs_path.unwrap())?;
-    //
-    // dbg!(&manifest);
+    let sz = size_of::<FieldMeta>();
 
-    let cargo_config = CargoConfig {
-        sysroot: Some(RustLibSource::Discover),
-        ..CargoConfig::default()
-    };
+    for section in file.sections() {
+        let data = section.data()?;
 
-    let load_cargo_config = LoadCargoConfig {
-        load_out_dirs_from_check: true,
-        with_proc_macro_server: ProcMacroServerChoice::Sysroot,
-        prefill_caches: true,
-    };
+        if !matches!(section.segment_name()?, Some(segment) if segment == "__gas_internals")
+            && !section.name()?.starts_with("__gas_internals")
+        {
+            continue;
+        }
 
-    let workspace = load_workspace_at(project_path, &cargo_config, &load_cargo_config, &|it| {
-        println!("loading: {}", it)
-    })?;
+        if data.len() % sz != 0 {
+            return Err(anyhow::anyhow!("invalid section size"));
+        }
 
-    println!("done loading");
+        println!("looking at: {}", section.name()?);
 
-    dbg!(&workspace);
+        for i in 0..(data.len() / sz) {
+            let f = &data[(i * sz)..((i + 1) * sz)];
+
+            assert_eq!(f.len(), sz);
+
+            let mut meta = MaybeUninit::<FieldMeta>::uninit();
+            let meta = unsafe {
+                std::ptr::copy_nonoverlapping(f.as_ptr(), meta.as_mut_ptr() as *mut u8, sz);
+                meta.assume_init()
+            };
+
+            dbg!(&meta);
+        }
+        break;
+    }
 
     Ok(())
 }
