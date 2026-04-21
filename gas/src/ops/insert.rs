d52d480599d6d70ba00dd91d2de091d5cc2420db
Antonio F. Trstenjak <antoniostignjedec@gmail.com>
2026-04-21 17:04:05 +0200

    Revert "Multi-insert parallelism"
    This reverts commit 2928f16f59d371b5ad8ae3d66f4951306f156777.

--- gas/src/ops/insert.rs
+++ gas/src/ops/insert.rs
@@ -2,7 +2,6 @@ use crate::connection::PgExecutor;
 use crate::internals::{PgParam, SqlQuery};
 use crate::model::ModelMeta;
 use crate::GasResult;
-use tokio::task::JoinSet;
 
 pub(crate) struct InsertOp<'a, T: ModelMeta> {
     // object will be replaced with the inserted one
@@ -24,17 +23,15 @@ impl<'a, T: ModelMeta> InsertOp<'a, T> {
 
         let (insert, returning) = T::gen_insert_parts_sql();
 
-        let mut values_chunks: Vec<(SqlQuery, Vec<PgParam>, usize)> =
-            vec![(SqlQuery::new(), vec![], 0)];
+        let mut values_chunks: Vec<(SqlQuery, Vec<PgParam>)> = vec![(SqlQuery::new(), vec![])];
 
         for (index, object) in self.objects.iter().enumerate() {
-            let (last_sql, last_params, last_count) =
-                values_chunks.last_mut().expect("chunks are not empty");
+            let (last_sql, last_params) = values_chunks.last_mut().expect("chunks are not empty");
 
             let (sql, params) = object.gen_insert_values_sql();
 
             if last_params.len() + params.len() > MAX_POSITIONAL_ARGS_LIMIT {
-                values_chunks.push((sql, params.to_vec(), 1));
+                values_chunks.push((sql, params.to_vec()));
                 continue;
             }
 
@@ -42,32 +39,26 @@ impl<'a, T: ModelMeta> InsertOp<'a, T> {
                 last_sql.append_str(",");
             }
 
-            *last_count += 1;
             last_sql.append_query(&sql);
             last_params.extend(params);
         }
 
-        let mut join_set = JoinSet::new();
-
         let mut index = 0;
-        for (sql, params, count) in values_chunks {
+        for (sql, params) in values_chunks {
             let mut full_query = insert.clone();
             full_query.append_query(&sql);
             full_query.append_query(&returning);
 
-            join_set.spawn(async move {
-                let rows = ctx.execute_parsed::<T>(full_query, &params).await?;
+            let rows = ctx.execute_parsed::<T>(full_query, &params).await?;
+            let rows_len = rows.len();
 
-                // NOTE: ordering should be same in practice, this might break sometime in the future
-                for (object, row) in self.objects[index..index + rows.len()].iter_mut().zip(rows) {
-                    *object = row;
-                }
-            });
+            // NOTE: ordering should be same in practice, this might break sometime in the future
+            for (object, row) in self.objects[index..index + rows.len()].iter_mut().zip(rows) {
+                *object = row;
+            }
 
-            index += count;
+            index += rows_len;
         }
-
-        join_set.join_all().await;
 
         Ok(())
     }
